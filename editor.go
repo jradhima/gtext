2b30ec658f61a42a4aa38e94b90e740c3409d8c2
Yannis Radhima <joaniradhima@gmail.com>
2025-06-05 18:36:51 +0200

    initial init

---
+++ editor.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+const VERSION = "0.0.1"
+const CTRL_Q rune = '\x11'
+const SPACE rune = '\x20'
+const CLEAR = "\x1b[2J"
+const CLEAR_RIGHT = "\x1b[K"
+const TOP_LEFT = "\x1b[H"
+const BOTTOM_RIGHT = "\x1b[999C\x1b[999B"
+const CURSOR_POSITION = "\x1b[6n"
+const HIDE_CURSOR = "\x1b[?25l"
+const SHOW_CURSOR = "\x1b[?25h"
+const INPUT_TIMEOUT = 100 * time.Millisecond
+const SHOW_NUMBERS = false
+
+type Editor struct {
+	reader    *bufio.Reader
+	config    EditorConfig
+	inputChan chan (ReadResult)
+}
+
+type EditorConfig struct {
+	ncol         int
+	nrow         int
+	cr           int
+	cc           int
+	showNumbers  bool
+	inputTimeout time.Duration
+}
+
+type ReadResult struct {
+	r   rune
+	err error
+}
+
+func NewEditorConfig(width int, height int) EditorConfig {
+	return EditorConfig{
+		ncol:         width,
+		nrow:         height,
+		cr:           1,
+		cc:           2,
+		showNumbers:  SHOW_NUMBERS,
+		inputTimeout: INPUT_TIMEOUT,
+	}
+}
+
+func NewEditor(r *os.File, config EditorConfig) *Editor {
+	return &Editor{
+		reader:    bufio.NewReader(r),
+		config:    config,
+		inputChan: make(chan ReadResult)}
+}
+
+func (e *Editor) moveCursor(r rune) {
+	switch r {
+	case 'w':
+		if e.config.cr > 1 {
+			e.config.cr--
+		}
+	case 's':
+		if e.config.cr < e.config.nrow-1 {
+			e.config.cr++
+		}
+	case 'a':
+		if e.config.cc > 2 {
+			e.config.cc--
+		}
+	case 'd':
+		if e.config.cc < e.config.ncol-1 {
+			e.config.cc++
+		}
+	}
+}
+
+func (e *Editor) drawRows(s string) string {
+	for i := range e.config.nrow {
+		if e.config.showNumbers {
+			s += fmt.Sprintf("%d", i+1)
+		} else {
+			s += "~"
+		}
+		if i == e.config.nrow/3 {
+			welcomeString := fmt.Sprintf("gtext editor -- version %s", VERSION)
+			padding := (e.config.ncol - len(welcomeString)) / 2
+			s += strings.Repeat(" ", padding) + welcomeString
+		}
+		s += CLEAR_RIGHT
+		if i != e.config.nrow-1 {
+			s += "\r\n"
+		}
+	}
+	s += fmt.Sprintf("\r\ncr: %d, cc %d", e.config.cr, e.config.cc)
+	return s
+}
+
+func (e *Editor) refreshScreen() {
+	ab := ""
+	ab += HIDE_CURSOR
+	ab += TOP_LEFT
+	ab = e.drawRows(ab)
+	ab += fmt.Sprintf(
+		"\x1b[%d;%dH",
+		e.config.cr,
+		e.config.cc)
+	ab += SHOW_CURSOR
+	fmt.Print(ab)
+}
+
+func (e *Editor) readKeyPresses() {
+	for {
+		r, _, err := e.reader.ReadRune()
+		e.inputChan <- ReadResult{r: r, err: err}
+		if err != nil {
+			return
+		}
+	}
+}
+
+func (e *Editor) processKeyPress(r rune) {
+	switch r {
+	case CTRL_Q:
+		shutdown("Ctrl+Q", 0)
+	case 'z':
+		shutdown("z", 0)
+	case 'w', 'a', 's', 'd':
+		e.moveCursor(r)
+	}
+}
+
+func (e *Editor) Start() {
+	go e.readKeyPresses()
+
+	for {
+		select {
+		case res := <-e.inputChan:
+			if res.err != nil {
+				shutdown(fmt.Sprintf("%s", res.err), 1)
+			}
+			e.processKeyPress(res.r)
+		case <-time.After(e.config.inputTimeout):
+		}
+		e.refreshScreen()
+	}
+}
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+
+	"golang.org/x/term"
+)
+
+func shutdown(s string, code int) {
+	fmt.Print(CLEAR)
+	fmt.Print(TOP_LEFT)
+
+	fmt.Printf("Exiting: %s\r\n", s)
+	time.Sleep(250 * time.Millisecond)
+
+	fmt.Print(CLEAR)
+	fmt.Print(TOP_LEFT)
+	os.Exit(code)
+}
+
+func getWindowSize() (int, int) {
+	ncol, nrow, err := term.GetSize(int(os.Stdout.Fd()))
+	if err != nil || (ncol == 0 && nrow == 0) {
+		return getWindowSizeFallback()
+	}
+	return ncol, nrow
+}
+
+func getWindowSizeFallback() (int, int) {
+	size, err := fmt.Print(BOTTOM_RIGHT)
+	if err != nil {
+		shutdown(fmt.Sprintf("%s", err), 1)
+	} else if size != 12 {
+		shutdown("Window size escape sequence error", 1)
+	}
+	return getCursorPosition()
+}
+
+func getCursorPosition() (int, int) {
+	size, err := fmt.Print(CURSOR_POSITION)
+	if err != nil {
+		shutdown(fmt.Sprintf("%s", err), 1)
+	} else if size != 4 {
+		shutdown("cursor position escape sequence error", 1)
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	b, err := reader.ReadBytes('R')
+	if err != nil {
+		shutdown(fmt.Sprintf("%s", err), 1)
+	} else if b[0] != '\x1b' || b[1] != '[' {
+		shutdown("cursor position return not valid", 1)
+	}
+	var nrow, ncol int
+	_, err = fmt.Sscanf(fmt.Sprintf("%s", b[1:]), "[%d;%dR", &nrow, &ncol)
+	if err != nil {
+		shutdown(fmt.Sprintf("error parsing: %s", err), 1)
+	}
+	return nrow, ncol
+}
+
+func main() {
+	fmt.Println("Initializing Text Editor...")
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic(err)
+	}
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
+
+	ncol, nrow := getWindowSize()
+
+	editorConfig := NewEditorConfig(ncol, nrow)
+	editor := NewEditor(os.Stdin, editorConfig)
+	editor.Start()
+}
